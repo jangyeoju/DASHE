8ea916063bac6f8e51fac2525656e15328ab1706
jang yeoju <duwn0105@naver.com>
2024-06-10 18:38:14 +0900

    log 추가3

--- create-credentials.js
+++ create-credentials.js
@@ -4,7 +4,7 @@ const path = require('path');
 if (process.env.SPREADSHEET_CREDENTIALS_JSON) {
   fs.writeFileSync(
     path.resolve(process.cwd(), 'spreadsheet-service-account.json'),
-    process.env.SPREADSHEET_CREDENTIALS_JSON
+    process.env.SPREADSHEET_CREDENTIALS_JSON.replace(/\\n/g, '\n')
   );
 } else {
   console.error(
